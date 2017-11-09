61aff4da502f7cc945d6cd3095d4b4e7b2851597
Guido Holz <guido@sportograf.com>
2017-11-09 19:21:43 +0100

    add repo

--- bg_sub.mk
+++ bg_sub.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 OPENCV_EXAMPLE_VERSION = 0.0.3
-OPENCV_EXAMPLE_SITE = $(call github,tobig77,opencv-example,$(OPENCV_EXAMPLE_VERSION))
+OPENCV_EXAMPLE_SITE = $(call github,guidoholz,bg_sub,$(OPENCV_EXAMPLE_VERSION))
 OPENCV_EXAMPLE_INSTALL_STAGING = YES
 OPENCV_EXAMPLE_INSTALL_TARGET = YES
 OPENCV_EXAMPLE_CONF_OPTS =
