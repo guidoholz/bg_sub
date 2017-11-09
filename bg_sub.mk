d5a03b2d422461ed021068f54ea5e12c09bd0a56
Guido Holz <guido@sportograf.com>
2017-11-09 21:32:56 +0100

    version

--- bg_sub.mk
+++ bg_sub.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCV_EXAMPLE_VERSION = 0.0.3
+OPENCV_EXAMPLE_VERSION = 0.0.4
 OPENCV_EXAMPLE_SITE = $(call github,guidoholz,bg_sub,$(OPENCV_EXAMPLE_VERSION))
 OPENCV_EXAMPLE_INSTALL_STAGING = YES
 OPENCV_EXAMPLE_INSTALL_TARGET = YES
